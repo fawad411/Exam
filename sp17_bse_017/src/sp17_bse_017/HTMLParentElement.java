d909777aa569a7ff9bbeb63f7af02d4bbbd9f515
fawad411 <fawadk17@gmail.com>
2020-06-10 17:57:58 +0500

    pattern

---
+++ sp17_bse_017/src/sp17_bse_017/HTMLParentElement.java
@@ -0,0 +1,51 @@
+package sp17_bse_017;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HTMLParentElement extends HTMLTag {
+	private String tagName;
+	private String startTag; 
+	private String endTag;
+	private List<HTMLTag>childrenTag;
+	
+	public HTMLParentElement(String tagName){
+		this.tagName = tagName;
+		this.startTag = "";
+		this.endTag = "";
+		this.childrenTag = new ArrayList<>();
+	}
+	public String getTagName() {
+		return tagName;
+	}
+
+	public void setStartTag(String tag) {
+		this.startTag = tag;
+	}
+
+	public void setEndTag(String tag) {
+		this.endTag = tag;
+	}
+
+	public void addChildTag(HTMLTag htmlTag){
+		childrenTag.add(htmlTag);
+	}
+	
+	public void removeChildTag(HTMLTag htmlTag){
+		childrenTag.remove(htmlTag);
+	}
+
+	public List<HTMLTag>getChildren(){
+		return childrenTag;
+	}
+
+	public void generateHtml() {
+		System.out.println(startTag);
+		for(HTMLTag tag : childrenTag){
+			tag.generateHtml();
+		}
+		System.out.println(endTag);
+		
+	}
+
+}
